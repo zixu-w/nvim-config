2920cd0a8c3c73563fc7acfe9a268e8ba22b0809
Zixu Wang <wangzixu.china@gmail.com>
2023-01-03 22:29:39 -0800

    [NFC][install] Always quote string variables
    Pedantically quote strings and variables.

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ if ! command -v brew &> /dev/null; then
 fi
 
 # Ensure we're in the correct working directory that contains this script.
-working_dir=$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")
-if [[ ${working_dir} != $(pwd) ]]; then
+working_dir="$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")"
+if [[ "${working_dir}" != "$(pwd)" ]]; then
   pushd -- "${working_dir}"
   trap popd EXIT
 fi
@@ -18,25 +18,25 @@ fi
 echo "Installing..."
 brew bundle
 
-nvim_config_path=${HOME}/.config/nvim
-current_link=$(readlink ${nvim_config_path} || true) # Ignore readlink failure.
+nvim_config_path="${HOME}/.config/nvim"
+current_link="$(readlink -- "${nvim_config_path}" || true)"
 
 # If ~/.config/nvim is already a symlink targeting us, we can skip it.
-if [[ ${current_link} == $(pwd)/nvim ]]; then
+if [[ "${current_link}" == "$(pwd)/nvim" ]]; then
   echo "'${nvim_config_path}' is already correctly linked, skip linking."
 else
   # Backup existing files.
-  if test -d ${nvim_config_path}; then
+  if test -d "${nvim_config_path}"; then
     echo "'${nvim_config_path}' already exists, backing up..."
-    timestamp=$(date +"%Y-%m-%d-%H-%M-%S")
-    backup_path=${nvim_config_path}_${timestamp}
-    mv ${nvim_config_path} ${backup_path}
+    timestamp="$(date +"%Y-%m-%d-%H-%M-%S")"
+    backup_path="${nvim_config_path}_${timestamp}"
+    mv -- "${nvim_config_path}" "${backup_path}"
     echo "Existing configuration backed up to '${backup_path}'"
   fi
 
   # Create a symlink at ~/.config/nvim that points to config files here.
   echo "Creating link to configuration..."
-  ln -sf $(pwd)/nvim ${nvim_config_path}
+  ln -sf -- "$(pwd)/nvim" "${nvim_config_path}"
 fi
 
 # Headlessly run the PackerSync command to initialize/update all plugins.
