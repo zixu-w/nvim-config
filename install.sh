a4b5a4793430706456baf9a0befb2fa15bc0039c
Zixu Wang <wangzixu.china@gmail.com>
2023-01-03 21:42:49 -0800

    [NFC][install] Add comments in install script
    NFC

--- install.sh
+++ install.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/env bash
 set -eu
 
+# Check for Homebrew.
 if ! command -v brew &> /dev/null; then
   echo "Homebrew is required!"
   exit 1
 fi
 
+# Install from Brewfile.
 echo "Installing..."
 brew bundle
 
 nvim_config_path=${HOME}/.config/nvim
-
 current_link=$(readlink ${nvim_config_path})
+
+# If ~/.config/nvim is already a symlink targeting us, we can skip it.
 if [[ ${current_link} == $(pwd)/nvim ]]; then
   echo "'${nvim_config_path}' is already correctly linked, skip linking."
 else
+  # Backup existing files.
   if test -d ${nvim_config_path}; then
     echo "'${nvim_config_path}' already exists, backing up..."
     timestamp=$(date +"%Y-%m-%d-%H-%M-%S")
@@ -23,9 +27,12 @@ else
     echo "Existing configuration backed up to '${backup_path}'"
   fi
 
+  # Create a symlink at ~/.config/nvim that points to config files here.
   echo "Creating link to configuration..."
   ln -sf $(pwd)/nvim ${nvim_config_path}
 fi
 
+# Headlessly run the PackerSync command to initialize/update all plugins.
+# Quit at the PackerComplete event.
 echo "Initializing Packer plugins..."
 nvim --headless -c "autocmd User PackerComplete quitall" -c "PackerSync"
