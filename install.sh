c255f80d043765c8ffabba13e43b0c4b6728a211
Zixu Wang <wangzixu.china@gmail.com>
2023-01-03 20:53:32 -0800

    [install] Skip linking if config is already linked to this repo
    Check if the config path `$HOME/.config/nvim` is already a symbolic link
    to the current repo, and skip backing up and linking again accordingly.

--- install.sh
+++ install.sh
@@ -11,16 +11,21 @@ brew bundle
 
 nvim_config_path=${HOME}/.config/nvim
 
-if test -d ${nvim_config_path}; then
-  echo "'${nvim_config_path}' already exists, backing up..."
-  timestamp=$(date +"%Y-%m-%d-%H-%M-%S")
-  backup_path=${nvim_config_path}_${timestamp}
-  mv ${nvim_config_path} ${backup_path}
-  echo "Existing configuration backed up to '${backup_path}'"
-fi
+current_link=$(readlink ${nvim_config_path})
+if [[ ${current_link} == $(pwd)/nvim ]]; then
+  echo "'${nvim_config_path}' is already correctly linked, skip linking."
+else
+  if test -d ${nvim_config_path}; then
+    echo "'${nvim_config_path}' already exists, backing up..."
+    timestamp=$(date +"%Y-%m-%d-%H-%M-%S")
+    backup_path=${nvim_config_path}_${timestamp}
+    mv ${nvim_config_path} ${backup_path}
+    echo "Existing configuration backed up to '${backup_path}'"
+  fi
 
-echo "Creating link to configuration..."
-ln -sf $(pwd)/nvim ${nvim_config_path}
+  echo "Creating link to configuration..."
+  ln -sf $(pwd)/nvim ${nvim_config_path}
+fi
 
 echo "Initializing Packer plugins..."
 nvim --headless -c "autocmd User PackerComplete quitall" -c "PackerSync"
